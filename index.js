36b063458b13c1711e3ceaf33808a956ba6f3338
Maera <baeitsmae@gmail.com>
2025-01-13 13:19:02 +0100

    smooth scroll

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let locations = []
 
 fetch("https://kasaa-app.netlify.app/datas/locations.json")
+//fetch("http://127.0.0.1:5500/datas/locations.json")
 .then(res => res.json())
 .then(datas => {
     locations = datas
